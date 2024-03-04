c1733ccf90b0d2b3a9960a9253f4633dbf0c6b0a
vladfedorov92 <vlad.fedorov.92@gmail.com>
2024-03-04 18:44:22 +0300

    spaces

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import {initializeApp} from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCB6zcSNsJzI7as4gxqteQq0qNqxb6GdDc",
-    authDomain: "card-game-4197c.firebaseapp.com",
-    projectId: "card-game-4197c",
-    storageBucket: "card-game-4197c.appspot.com",
-    messagingSenderId: "611679251374",
-    appId: "1:611679251374:web:a9ee96793dc66fc09ff63f"
+  apiKey: 'AIzaSyCB6zcSNsJzI7as4gxqteQq0qNqxb6GdDc',
+  authDomain: 'card-game-4197c.firebaseapp.com',
+  projectId: 'card-game-4197c',
+  storageBucket: 'card-game-4197c.appspot.com',
+  messagingSenderId: '611679251374',
+  appId: '1:611679251374:web:a9ee96793dc66fc09ff63f',
 };
 
 // Initialize Firebase
